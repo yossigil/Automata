5f2d06d904611eac244c3690ba95062d6da41a10
Yossi Gil <yogi@cs.technion.ac.il>
2021-07-08 20:16:14 +0300

    Renamsing tests

--- test/finite/Demo.java
+++ test/finite/Demo.java
@@ -14,10 +14,16 @@ class Demo {
   final Text x2 = x1.star().Or('c').then(abStar.Then('c').Then('a'));
   final Text x3 = x2.then(a_b).then(x2).star();
   final Text x4 = x3.then(x3).star().or(x2).star().then(x1);
+  final Text abcd = new Text(new Text('a').then(new Text('b').then(new Text('c')).then(new Text('d'))));
 
-  @Test void exists() {
+  @Test void t1() {
+   System.out.println(abcd.TikZ()); 
+   System.out.println(abcd.DFSA().TikZ()); 
+  }
+  @Test void t2() {
     System.out.println(x4.TikZ());
     DFSA<Character> d = x4.DFSA();
     System.out.println(d.TikZ());
+    System.out.println(d.minimize().TikZ());
   }
 }
