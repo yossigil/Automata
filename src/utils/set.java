d070fd14cf1f91f2b04b4839ceb77c32229a62eb
Yossi Gil <yogi@cs.technion.ac.il>
2021-07-09 16:30:13 +0300

    Use addAll rather than loop

--- src/utils/set.java
+++ src/utils/set.java
@@ -1,43 +1,41 @@
 package utils;
 
+import java.util.Collections;
 import java.util.Set;
 
 public enum set {
   ;
-  @SafeVarargs public static <T> Set<T> of(T... ts) {
-    Set<T> $ = empty.Set();
-    for (T t : ts)
-      $.add(t);
+  @SafeVarargs public static <T> Set<T> of(final T... ts) {
+    final Set<T> $ = empty.Set();
+    Collections.addAll($, ts);
     return $;
   }
 
-  public static <T> T pick(Iterable<T> ts) {
-    for (T t : ts)
-      return t;
+  public static <T> T pick(final Iterable<T> ts) {
+    for (final T t : ts) return t;
     return null;
   }
 
-  public static <T> Set<T> of(Iterable<T> ts) {
-    Set<T> $ = empty.Set();
-    for (T t : ts)
-      $.add(t);
+  public static <T> Set<T> of(final Iterable<T> ts) {
+    final Set<T> $ = empty.Set();
+    for (final T t : ts) $.add(t);
     return $;
   }
 
-  public static <T> Set<T> minus(Iterable<T> qs1, Set<T> sq2) {
-    Set<T> $ = set.of(qs1);
+  public static <T> Set<T> minus(final Iterable<T> qs1, final Set<T> sq2) {
+    final Set<T> $ = set.of(qs1);
     $.removeAll(sq2);
     return $;
   }
 
-  public static <T> Set<T> intersection(Set<T> s1, Set<T> s2) {
-    Set<T> $ = set.of(s1);
+  public static <T> Set<T> intersection(final Set<T> s1, final Set<T> s2) {
+    final Set<T> $ = set.of(s1);
     $.retainAll(s2);
     return $;
   }
 
-  public static <T> Set<T> union(Set<T> s1, Set<T> s2) {
-    Set<T> $ = set.of(s1);
+  public static <T> Set<T> union(final Set<T> s1, final Set<T> s2) {
+    final Set<T> $ = set.of(s1);
     $.addAll(s2);
     return $;
   }
