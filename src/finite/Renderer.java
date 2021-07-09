a65b3fb4de91beeccafa8a04b98bd0ac99eb0983
Yossi Gil <yogi@cs.technion.ac.il>
2021-07-09 17:46:32 +0300

    make method static

--- src/finite/Renderer.java
+++ src/finite/Renderer.java
@@ -6,8 +6,9 @@ import utils.empty;
 
 abstract class Renderer extends Texter { //@formatter:off
   final String render() { return wrap(traverse()); }
-  final String wrap(final String ¢) { return sprintf("graph{\n%s}\n", ¢); }
+  static String wrap(final String ¢) { return sprintf("graph{\n%s}\n", ¢); }
   abstract String traverse();
   final Set<Q> elaborated = empty.Set();
   boolean elaborated(final Q ¢) { return elaborated.contains(¢); }
+  static String square(String $) { return $.isBlank() ? "" : "[" + $ + "]"; }
 }
