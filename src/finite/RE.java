13f3dd6dde91e058700bd1a9235d33eeec395caa
Yossi Gil <yogi@cs.technion.ac.il>
2021-07-09 16:38:12 +0300

    Add @override and format

--- src/finite/RE.java
+++ src/finite/RE.java
@@ -3,20 +3,17 @@ package finite;
 public abstract class RE {
 //@formatter:off
   abstract static class Nullary extends RE {}
-  abstract static class Unary extends RE { final RE x; Unary(RE x) { this.x = x; } }
-  abstract static class Binary extends RE {
-	  final RE x1,x2;
-	  Binary(RE x1, RE x2) { this.x1 = x1; this.x2 = x2; }
-  }
+  abstract static class Unary extends RE { final RE x; Unary(final RE x) { this.x = x; } }
+  abstract static class Binary extends RE { final RE x1,x2; Binary(final RE x1, final RE x2) { this.x1 = x1; this.x2 = x2; } }
   abstract static class Φ extends Nullary{ }
   abstract static class ε extends Nullary{}
-  abstract static class Char extends Nullary { public final char c; public Char(char c) { this.c = c; } }
-  abstract static class Star extends Unary{ public Star(RE r) { super(r);}}
-  abstract static class Plus extends Unary{ public Plus(RE r) { super(r);}}
-  abstract static class Not extends Unary{ public Not(RE r) { super(r);}}
-  abstract static class And extends RE.Binary{public And(RE x1, RE x2) { super(x1, x2); } }
-  abstract static class Or extends RE.Binary{ public Or(RE x1, RE x2) { super(x1, x2); }}
-  abstract static class Then extends RE.Binary{ Then(RE x1, RE x2) {super (x1,x2); }}
+  abstract static class Char extends Nullary { public final char c; public Char(final char c) { this.c = c; } }
+  abstract static class Star extends Unary{ public Star(final RE r) { super(r);}}
+  abstract static class Plus extends Unary{ public Plus(final RE r) { super(r);}}
+  abstract static class Not extends Unary{ public Not(final RE r) { super(r);}}
+  abstract static class And extends RE.Binary{public And(final RE x1, final RE x2) { super(x1, x2); } }
+  abstract static class Or extends RE.Binary{ public Or(final RE x1, final RE x2) { super(x1, x2); }}
+  abstract static class Then extends RE.Binary{ Then(final RE x1, final RE x2) {super (x1,x2); }}
   abstract <T> T reduce(Reducer<T> r);
   //@formatter:off
   interface Reducer<T> {
@@ -30,65 +27,47 @@ public abstract class RE {
     T and(T t1, T t2);
   }
 
+  // Factories
   public static RE ε() {
-    return new ε() {
-      public <T> T reduce(Reducer<T> ¢) {
-        return ¢.ε();
-      }
-    };
+    return new ε() { @Override public <T> T reduce(final Reducer<T> ¢) { return ¢.ε(); } };
   }
 
   public static RE Φ() {
-    return new Φ() {
-      public <T> T reduce(Reducer<T> ¢) {
-        return ¢.Φ();
-      }
-    };
+    return new Φ() { @Override public <T> T reduce(final Reducer<T> ¢) { return ¢.Φ(); } };
   }
 
-  public static RE c(char c) {
-    return new Char(c) {
-      public <T> T reduce(Reducer<T> ¢) {
-        return ¢.σ(c);
-      }
-    };
+  public static RE c(final char c) {
+    return new Char(c) { @Override public <T> T reduce(final Reducer<T> ¢) { return ¢.σ(c); } };
   }
 
   public final Star star() {
-    return new Star(RE.this) {
-      public <T> T reduce(Reducer<T> ¢) {
-        return ¢.star(x.reduce(¢));
-      }
-    };
+    return new Star(this) { @Override public <T> T reduce(final Reducer<T> ¢) { return ¢.star(x.reduce(¢)); } };
   }
 
   public final Not not() {
-    return new Not(RE.this) {
-      public <T> T reduce(Reducer<T> ¢) {
+    return new Not(this) {
+      @Override
+      public <T> T reduce(final Reducer<T> ¢) {
         return ¢.not(x.reduce(¢));
       }
     };
   }
 
-  public final Or or(RE x) {
-    return new Or(RE.this, x) {
-      public <T> T reduce(Reducer<T> ¢) {
-        return ¢.or(x1.reduce(¢), x2.reduce(¢));
-      }
+  public final Or or(final RE x) {
+    return new Or(this, x) {
+      @Override public <T> T reduce(final Reducer<T> ¢) { return ¢.or(x1.reduce(¢), x2.reduce(¢)); }
     };
   }
 
-  public final Then then(RE x) {
-    return new Then(RE.this, x) {
-      public <T> T reduce(Reducer<T> ¢) {
-        return ¢.then(x1.reduce(¢), x2.reduce(¢));
-      }
+  public final Then then(final RE x) {
+    return new Then(this, x) { 
+      @Override public <T> T reduce(final Reducer<T> ¢) { return ¢.then(x1.reduce(¢), x2.reduce(¢)); } 
     };
   }
 
-  public final And and(RE x) {
-    return new And(RE.this, x) {
-      public <T> T reduce(Reducer<T> ¢) {
+  public final And and(final RE x) {
+    return new And(this, x) {
+      @Override public <T> T reduce(final Reducer<T> ¢) {
         return ¢.and(x1.reduce(¢), x2.reduce(¢));
       }
     };
@@ -97,32 +76,27 @@ public abstract class RE {
 //@formatter:off
   public final RE dup() {
     return reduce(new Reducer<RE>() {
-      public RE ε() { return RE.ε(); }
-      public RE Φ() { return RE.Φ(); }
-      public RE σ(char ¢) { return RE.c(¢); }
-      public RE not(RE x) { return x.not(); }
-      public RE star(RE x) { return x.star(); }
-      public RE then(RE x1, RE x2) { return x1.then(x2); }
-      public RE or(RE x1, RE x2) { return x1.or(x2); }
-      public RE and(RE x1, RE x2) { return x1.and(x2); }
+      @Override public RE ε() { return RE.ε(); }
+      @Override public RE Φ() { return RE.Φ(); }
+      @Override public RE σ(final char ¢) { return RE.c(¢); }
+      @Override public RE not(final RE x) { return x.not(); }
+      @Override public RE star(final RE x) { return x.star(); }
+      @Override public RE then(final RE x1, final RE x2) { return x1.then(x2); }
+      @Override public RE or(final RE x1, final RE x2) { return x1.or(x2); }
+      @Override public RE and(final RE x1, final RE x2) { return x1.and(x2); }
     });
   }
 
   public final <Σ> NFSA<Σ> fsa() {
     return reduce(new Reducer<NFSA<Σ>>() {
-      @Override public NFSA<Σ> 
-      Φ() { return NFSA.Φ(); }
+      @Override public NFSA<Σ> Φ() { return NFSA.Φ(); }
       @Override public NFSA<Σ> ε() { return NFSA.ε(); }
-      @Override
-      @SuppressWarnings("unchecked") public NFSA<Σ> σ(char σ) {
-        return NFSA.σ((Σ) (Character) σ);
-      }
-      @Override public NFSA<Σ> star(NFSA<Σ> ¢) { return ¢.star(); }
-      @Override public NFSA<Σ> not(NFSA<Σ> ¢) { return ¢.not(); }
-      @Override public NFSA<Σ> then(NFSA<Σ> a1, NFSA<Σ> a2) { return a1.then(a2); }
-      @Override public NFSA<Σ> or(NFSA<Σ> a1, NFSA<Σ> a2) { return a1.or(a2); }
-      @Override public NFSA<Σ> and(NFSA<Σ> a1, NFSA<Σ> a2) { return a1.and(a2); }
+      @Override @SuppressWarnings("unchecked") public NFSA<Σ> σ(final char σ) { return NFSA.σ((Σ) (Character) σ); }
+      @Override public NFSA<Σ> star(final NFSA<Σ> ¢) { return ¢.star(); }
+      @Override public NFSA<Σ> not(final NFSA<Σ> ¢) { return ¢.not(); }
+      @Override public NFSA<Σ> then(final NFSA<Σ> a1, final NFSA<Σ> a2) { return a1.then(a2); }
+      @Override public NFSA<Σ> or(final NFSA<Σ> a1, final NFSA<Σ> a2) { return a1.or(a2); }
+      @Override public NFSA<Σ> and(final NFSA<Σ> a1, final NFSA<Σ> a2) { return a1.and(a2); }
     });
   }
-
 }
