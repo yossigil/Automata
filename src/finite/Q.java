825a147b05a59ef07df490aecf344389452da088
Yossi Gil <yogi@cs.technion.ac.il>
2021-07-09 17:46:07 +0300

    Use pre-increment

--- src/finite/Q.java
+++ src/finite/Q.java
@@ -2,7 +2,7 @@ package finite;
 
 //@formatter:off
 class Q {
-  static int count;
-  final int n = count++;
+  static int count = -1;
+  final int n = ++count;
   @Override public String toString() { return "ι" + n; }
 }
