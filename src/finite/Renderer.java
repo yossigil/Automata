cd923271f75cd6f858190a75cf1e7e4d2b0a1993
Yossi Gil <yogi@cs.technion.ac.il>
2021-07-09 16:30:40 +0300

    Add final

--- src/finite/Renderer.java
+++ src/finite/Renderer.java
@@ -6,8 +6,8 @@ import utils.empty;
 
 abstract class Renderer extends Texter { //@formatter:off
   final String render() { return wrap(traverse()); }
-  final String wrap(String ¢) { return sprintf("graph{\n%s}\n", ¢); }
+  final String wrap(final String ¢) { return sprintf("graph{\n%s}\n", ¢); }
   abstract String traverse();
   final Set<Q> elaborated = empty.Set();
-  boolean elaborated(Q ¢) { return elaborated.contains(¢); }
+  boolean elaborated(final Q ¢) { return elaborated.contains(¢); }
 }
