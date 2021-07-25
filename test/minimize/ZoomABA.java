c0f448c9ed677e679a274881f34ea1c3edb952a8
Yossi Gil <yogi@cs.technion.ac.il>
2021-07-25 16:51:53 +0300

    Fix test names

--- test/minimize/ZoomABA.java
+++ test/minimize/ZoomABA.java
@@ -1,5 +1,6 @@
 package minimize;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import automaton.FSA;
@@ -9,21 +10,23 @@ public class ZoomABA {
   {
     Sample.aba.show();
   }
-  @Test void ε() { assert !dfsa.run(""); }
-  @Test void ab() { assert !dfsa.run("ab"); }
-  @Test void abb() { assert !dfsa.run("abb"); }
-  @Test void aba() { assert !dfsa.run("aba"); }
-  @Test void abc() { assert dfsa.run("aba"); }
-  @Test void a() { assert !dfsa.run("a"); }
-  @Test void b() { assert !dfsa.run("b"); }
-  @Test void aa() { assert !dfsa.run("aa"); }
-  @Test void bb() { assert !dfsa.run("bb"); }
-  @Test void ba() { assert !dfsa.run("ba"); }
-  @Test void aab() { assert !dfsa.run("aab"); }
-  @Test void bab() { assert !dfsa.run("bab"); }
-  @Test void aaa() { assert !dfsa.run("aaa"); }
-  @Test void bbb() { assert !dfsa.run("bbb"); }
-  @Test void abababa() { assert !dfsa.run("abababa"); }
-  @Test void abaaba() { assert dfsa.run("abaaba"); }
-  @Test void ababab() { assert !dfsa.run("ababab"); }
+  @Nested class DFSA {
+    @Test void ε() { assert !dfsa.run(""); }
+    @Test void ab() { assert !dfsa.run("ab"); }
+    @Test void abb() { assert !dfsa.run("abb"); }
+    @Test void aba() { assert !dfsa.run("aba"); }
+    @Test void abc() { assert dfsa.run("aba"); }
+    @Test void a() { assert !dfsa.run("a"); }
+    @Test void b() { assert !dfsa.run("b"); }
+    @Test void aa() { assert !dfsa.run("aa"); }
+    @Test void bb() { assert !dfsa.run("bb"); }
+    @Test void ba() { assert !dfsa.run("ba"); }
+    @Test void aab() { assert !dfsa.run("aab"); }
+    @Test void bab() { assert !dfsa.run("bab"); }
+    @Test void aaa() { assert !dfsa.run("aaa"); }
+    @Test void bbb() { assert !dfsa.run("bbb"); }
+    @Test void abababa() { assert !dfsa.run("abababa"); }
+    @Test void abaaba() { assert !dfsa.run("abaaba"); }
+    @Test void ababab() { assert !dfsa.run("ababab"); }
+  }
 }
