341ae7b6c8e5ea70fdc6ce62ef56f0e30c1dd92a
Yossi Gil <yogi@cs.technion.ac.il>
2021-07-26 13:05:10 +0300

    Fix tests

--- test/minimize/ZoomABA.java
+++ test/minimize/ZoomABA.java
@@ -6,27 +6,70 @@ import org.junit.jupiter.api.Test;
 import automaton.FSA;
 
 public class ZoomABA {
-  final FSA<Character> dfsa = Sample.aba.DFSA();
+  final Sample $ = Sample.aba;
   {
-    Sample.aba.show();
+    $.show();
+  }
+  @Nested class NFSA {
+    FSA<Character> $ = ZoomABA.this.$.NFSA();
+    @Test void ε() { assert !$.run(""); }
+    @Test void ab() { assert !$.run("ab"); }
+    @Test void abb() { assert !$.run("abb"); }
+    @Test void aba() { assert $.run("aba"); }
+    @Test void abc() { assert !$.run("abc"); }
+    @Test void a() { assert !$.run("a"); }
+    @Test void b() { assert !$.run("b"); }
+    @Test void aa() { assert !$.run("aa"); }
+    @Test void bb() { assert !$.run("bb"); }
+    @Test void ba() { assert !$.run("ba"); }
+    @Test void aab() { assert !$.run("aab"); }
+    @Test void bab() { assert !$.run("bab"); }
+    @Test void aaa() { assert !$.run("aaa"); }
+    @Test void bbb() { assert !$.run("bbb"); }
+    @Test void abababa() { assert !$.run("abababa"); }
+    @Test void abaaba() { assert !$.run("abaaba"); }
+    @Test void ababab() { assert !$.run("ababab"); }
   }
   @Nested class DFSA {
-    @Test void ε() { assert !dfsa.run(""); }
-    @Test void ab() { assert !dfsa.run("ab"); }
-    @Test void abb() { assert !dfsa.run("abb"); }
-    @Test void aba() { assert !dfsa.run("aba"); }
-    @Test void abc() { assert dfsa.run("aba"); }
-    @Test void a() { assert !dfsa.run("a"); }
-    @Test void b() { assert !dfsa.run("b"); }
-    @Test void aa() { assert !dfsa.run("aa"); }
-    @Test void bb() { assert !dfsa.run("bb"); }
-    @Test void ba() { assert !dfsa.run("ba"); }
-    @Test void aab() { assert !dfsa.run("aab"); }
-    @Test void bab() { assert !dfsa.run("bab"); }
-    @Test void aaa() { assert !dfsa.run("aaa"); }
-    @Test void bbb() { assert !dfsa.run("bbb"); }
-    @Test void abababa() { assert !dfsa.run("abababa"); }
-    @Test void abaaba() { assert !dfsa.run("abaaba"); }
-    @Test void ababab() { assert !dfsa.run("ababab"); }
+    FSA<Character> $ = ZoomABA.this.$.DFSA();
+    @Test void ε() { assert !$.run(""); }
+    @Test void ab() { assert !$.run("ab"); }
+    @Test void abb() { assert !$.run("abb"); }
+    @Test void aba() { assert $.run("aba"); }
+    @Test void abc() { assert $.run("aba"); }
+    @Test void a() { assert !$.run("a"); }
+    @Test void b() { assert !$.run("b"); }
+    @Test void aa() { assert !$.run("aa"); }
+    @Test void bb() { assert !$.run("bb"); }
+    @Test void ba() { assert !$.run("ba"); }
+    @Test void aab() { assert !$.run("aab"); }
+    @Test void bab() { assert !$.run("bab"); }
+    @Test void aaa() { assert !$.run("aaa"); }
+    @Test void bbb() { assert !$.run("bbb"); }
+    @Test void abababa() { assert !$.run("abababa"); }
+    @Test void abaaba() { assert $.run("abaaba"); }
+    @Test void ababab() { assert !$.run("ababab"); }
   }
+
+  @Nested class MDFSA {
+    FSA<Character> $ = ZoomABA.this.$.MDFSA();
+    @Test void ε() { assert !$.run(""); }
+    @Test void ab() { assert !$.run("ab"); }
+    @Test void abb() { assert !$.run("abb"); }
+    @Test void aba() { assert !$.run("aba"); }
+    @Test void abc() { assert $.run("aba"); }
+    @Test void a() { assert !$.run("a"); }
+    @Test void b() { assert !$.run("b"); }
+    @Test void aa() { assert !$.run("aa"); }
+    @Test void bb() { assert !$.run("bb"); }
+    @Test void ba() { assert !$.run("ba"); }
+    @Test void aab() { assert !$.run("aab"); }
+    @Test void bab() { assert !$.run("bab"); }
+    @Test void aaa() { assert !$.run("aaa"); }
+    @Test void bbb() { assert !$.run("bbb"); }
+    @Test void abababa() { assert !$.run("abababa"); }
+    @Test void abaaba() { assert $.run("abaaba"); }
+    @Test void ababab() { assert !$.run("ababab"); }
+  }
+
 }
