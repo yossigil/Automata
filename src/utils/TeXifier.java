90a71ab5b3d3106de36305b09da27bee68687ecc
Yossi Gil <yogi@cs.technion.ac.il>
2021-07-27 20:01:00 +0300

    add a stack class

--- src/utils/TeXifier.java
+++ src/utils/TeXifier.java
@@ -1,7 +1,26 @@
 package utils;
 
-@SuppressWarnings("static-method")
-public class TeXifier extends Tabulator {
-  protected String math(String ¢) { return "\\ensuremath{" + ¢ + "}"; }
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("static-method") public class TeXifier extends Tabulator {
+  Stack stack = new Stack();
   protected String tt(String ¢) { return "\\text{" + ¢ + "}"; }
+  protected String math(String ¢) { return "\\ensuremath{" + ¢ + "}"; }
+  void begin(String ¢) { 
+    printf("\\begin{%s}", ¢); 
+    stack.push(¢);
+  }
+  void end(String ¢) { 
+    printf("\\end{%s}", ¢); 
+    }
+}
+
+class Stack {
+  final List<String> stack = empty.List();
+  boolean isEmpty() { return stack.isEmpty(); }
+  void push(String value) { stack.add(value); }
+  String pop() { return stack.remove(last()); }
+  String peek() { return stack.get(last()); }
+  private int last() { return stack.size() - 1; }
 }
