ed35607ebf9bcb5135210665db0a15469b8eaf7a
Yossi Gil <yogi@cs.technion.ac.il>
2021-07-27 07:43:28 +0300

    Fix recurring state bug

--- src/automaton/Thompson.java
+++ src/automaton/Thompson.java
@@ -2,7 +2,9 @@ package automaton;
 
 enum Thompson {
   ;
-  static <Σ> NFSA<Σ> or(NFSA<Σ> a1, final NFSA<Σ> a2) {
+  static <Σ> NFSA<Σ> or(NFSA<Σ> n1, final NFSA<Σ> n2) {
+    NFSA<Σ> a1 = n1.refresh();
+    NFSA<Σ> a2 = n2.refresh();
     final Q $ = new Q();
     return NFSA.<Σ>builder($)//
         .ε($, a1.q0) //
@@ -13,15 +15,17 @@ enum Thompson {
         .build();
   }
   static <Σ> NFSA<Σ> and(final NFSA<Σ> a1, final NFSA<Σ> a2) {
-    Q $ = new Q();
-    return NFSA.<Σ>builder($).ζ(a1).ζ(a2).ε($, a1.q0).ε($, a1.q0).build();
+    throw new UnsupportedOperationException("26 Jul 2021");
   }
-  static <Σ> NFSA<Σ> many(NFSA<Σ> a) {
+  static <Σ> NFSA<Σ> many(NFSA<Σ> n) {
+    NFSA<Σ> a = n.refresh();
     final var $ = NFSA.<Σ>builder(a.q0).ε(a.q0, a.q0).ζ(a.q0).Δ(a).ε(a);
     for (final Q q : a.ζ) $.ε(q, $.q0());
     return $.build();
   }
-  static <Σ> NFSA<Σ> then(NFSA<Σ> a1, NFSA<Σ> a2) {
+  static <Σ> NFSA<Σ> then(NFSA<Σ> n1, NFSA<Σ> n2) {
+    NFSA<Σ> a1 = n1.refresh();
+    NFSA<Σ> a2 = n2.refresh();
     final var $ = NFSA.<Σ>builder(a1.q0).ε(a1).ε(a2).Δ(a1).Δ(a2).ζ(a2);
     for (final Q q : a1.ζ) $.ε(q, a2.q0);
     return $.build();
